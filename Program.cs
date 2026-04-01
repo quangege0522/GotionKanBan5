e65ef93e8ecaca25a53f65d8bd5a3231c0aa5dea
yq <m18256978246_1@163.com>
2026-04-01 08:06:10 +0800

    国轩高科5厂看板测试版本项目搭建

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ✅ 1. MVC + Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MES Production API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MES Production API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
